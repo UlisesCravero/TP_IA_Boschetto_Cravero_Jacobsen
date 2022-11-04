44f4a4ac2f0a6e3160cd3d6680cd237228e99785
Ulises Cravero <ulisescravero95@gmail.com>
2022-11-03 21:33:04 -0300

    restriccion de 2 cajas adyacentes

--- entrega2.py
+++ entrega2.py
@@ -1,18 +1,19 @@
 from itertools import combinations
-from simpleai.search import CspProblem, backtrack, MOST_CONSTRAINED_VARIABLE
+from simpleai.search import MOST_CONSTRAINED_VARIABLE, CspProblem, backtrack,LEAST_CONSTRAINING_VALUE,HIGHEST_DEGREE_VARIABLE
 import re
 
 def armar_mapa( filas, columnas, cantidad_paredes, cantidad_cajas_objetivos):
-    
+    #domains["fila"] = numero
     #mapa_resultante =  armar_mapa(5,4,3,2)
-    if filas != columnas:
-        print("es rectangulo") #CAMBIAR
-
-    #problem_variables = []
+    #if filas != columnas: #son variables?
+    #    print("es rectangulo") # hay que sumar filas y colmunas como variables y controlar que sean enteros positivos distintos?
+   
     CAJAS = []
     OBJETIVOS = []
     PAREDES = []
- 
+    constraints = []
+    domains = {}
+    
     for pared in list(range(cantidad_paredes)):
         PAREDES.append("pared{}".format(pared+1))
 
@@ -23,40 +24,31 @@ def armar_mapa( filas, columnas, cantidad_paredes, cantidad_cajas_objetivos):
 
     problem_variables = PAREDES + OBJETIVOS + CAJAS + ["PJ"]
 
-    domains = {}
-
+    
     lista_columnas = list(range(columnas))
     lista_filas = list(range(filas))
-
     casilleros = [
         (fil, col)
         for fil in lista_filas
         for col in lista_columnas
     ]
 
-
     for variable in problem_variables:
         domains[variable] = list(casilleros)
-
-    
-    constraints = []
+ 
 
     def different(problem_variables, values):
         celda1, celda2 = values
         return celda1 != celda2
- 
    
     # las cajas, paredes y pj no pueden estar en el mismo lugar
     PJ_CAJAS_PAREDES = ["PJ"] + CAJAS + PAREDES
-    #print("pj con cajas y paredes")
-    #print(PJ_CAJAS_PAREDES)
+
     for celda1, celda2 in combinations(PJ_CAJAS_PAREDES,2):
         constraints.append(((celda1, celda2), different))
 
     # los objetivos no deben estar arriba de una una pared 
     OBJ_PAREDES = OBJETIVOS + PAREDES
-    #print("objtivos y paredes")
-    #print(OBJ_PAREDES)
     for celda1, celda2 in combinations(OBJ_PAREDES,2):
         constraints.append(((celda1, celda2), different))
 
@@ -94,46 +86,81 @@ def armar_mapa( filas, columnas, cantidad_paredes, cantidad_cajas_objetivos):
     CAJAS_OBJETIVOS = CAJAS + OBJETIVOS
     constraints.append((CAJAS_OBJETIVOS, game_not_win))   
     
-    # Restriccion de mas de una pared adyacente
-    def dos_paredes_adyacentes_caja(variables,values):
-        aux_cajas = []       
-        aux_paredes = []    
-        #cantidad_co = 0
-        
-        for idx, vari in enumerate(variables):
-            # expresión regular para saber si es caja               
-            if re.search("^caja[0-9]+", vari):
-                aux_cajas.append(values[idx])
-            elif re.search("^pared[0-9]+", vari):
-                aux_paredes.append(values[idx])
-        for caja in aux_cajas:
-            ady_caja = [
-                (caja[0]+1,caja[1]), #abajo
-                (caja[0]-1,caja[1]), #arriba
-                (caja[0],caja[1]+1), #derecha
-                (caja[0],caja[1]-1)  #izquierda
-            ]
-            movimientos = []
-            for f,c in ady_caja:
-                if f>=0 and f<filas and c>=0 and c<filas:
-                    movimientos.append((f,c))
-
-            cont_paredes_ady = 0
-            for mov in movimientos:
-                if mov in aux_paredes:
-                    cont_paredes_ady += 1
-        
-            if cont_paredes_ady >= 2:
-                return False
-            elif len(movimientos) < 4 and cont_paredes_ady >= 1:
-                return False
-            else:
+    #funcion que devuelve true si es adyacente
+    def es_adyacente(pos,ady):        
+        lista_adyacentes = calculo_adyacentes(pos)
+        for x in lista_adyacentes:
+            if ady == x:
                 return True
+        return False
 
-    CAJAS_PAREDES = CAJAS + PAREDES
-    constraints.append((CAJAS_PAREDES,dos_paredes_adyacentes_caja))   
+    def calculo_adyacentes(pos):
+        adyacentes = [
+            (pos[0]+1,pos[1]), #abajo
+            (pos[0]-1,pos[1]), #arriba
+            (pos[0],pos[1]+1), #derecha
+            (pos[0],pos[1]-1)  #izquierda
+        ]
+        movimientos = []
+        for f,c in adyacentes:
+            if f>=0 and f<filas and c>=0 and c<filas:
+                movimientos.append((f,c))
+        return movimientos
 
-    # Restriccion de no ubicar cajas en esquinas
+    def caja_mas_de_2_paredes_adyacentes(variables,values):
+        #recibo 1 caja seguida de todas las paredes
+        c = 0
+        caja = values[0]
+        for pared in values[1:]:
+            if es_adyacente(caja,pared):
+                c+=1
+        if c > 1:
+            return False
+        elif c == 1 and len(calculo_adyacentes(caja)) < 4:
+            return False
+        else:
+            return True
+
+    # for celda1, celda2 in combinations(OBJ_PAREDES,2):
+    #     constraints.append(((celda1, celda2), different))
+    for caja in CAJAS:
+        if len(PAREDES) > 1:
+            for celda1, celda2 in combinations(PAREDES,2):
+                constraints.append((([caja] + [celda1]+ [celda2]), caja_mas_de_2_paredes_adyacentes))
+        else:
+            constraints.append((([caja]+ PAREDES), caja_mas_de_2_paredes_adyacentes))
+
+    # Restriccion de mas de una pared adyacente
+    # def dos_paredes_adyacentes_caja(variables,values):
+    #     aux_cajas = []       
+    #     aux_paredes = []    
+    #     #cantidad_co = 0
+        
+    #     for idx, vari in enumerate(variables):
+    #         # expresión regular para saber si es caja               
+    #         if re.search("^caja[0-9]+", vari):
+    #             aux_cajas.append(values[idx])
+    #         elif re.search("^pared[0-9]+", vari):
+    #             aux_paredes.append(values[idx])
+    #     for caja in aux_cajas:
+            
+
+    #         cont_paredes_ady = 0
+    #         for mov in movimientos:
+    #             if mov in aux_paredes:
+    #                 cont_paredes_ady += 1
+        
+    #         if cont_paredes_ady >= 2:
+    #             return False
+    #         elif len(movimientos) < 4 and cont_paredes_ady >= 1:
+    #             return False
+    #         else:
+    #             return True
+
+    # CAJAS_PAREDES = CAJAS + PAREDES
+    # constraints.append((CAJAS_PAREDES,dos_paredes_adyacentes_caja))   
+
+    # Restriccion de no ubicar cajas en esquinas   
     for caja in CAJAS:       
         if (0,0) in domains[caja]:
             domains[caja].remove((0,0))
@@ -149,11 +176,11 @@ def armar_mapa( filas, columnas, cantidad_paredes, cantidad_cajas_objetivos):
     
     
     problema = CspProblem(problem_variables, domains, constraints)
-    resultado = backtrack(problema,variable_heuristic=MOST_CONSTRAINED_VARIABLE)
+    resultado = backtrack(problema,variable_heuristic=LEAST_CONSTRAINING_VALUE)
     #print(resultado)
     lista_cajas = []
     lista_objetivos = []
-    lista_paredes = []
+    lista_paredes = [] 
     
     for key in resultado.keys():
         if re.search("^caja[0-9]+", key):
@@ -164,4 +191,4 @@ def armar_mapa( filas, columnas, cantidad_paredes, cantidad_cajas_objetivos):
             lista_objetivos.append(resultado[key])        
            
     return (lista_paredes, lista_cajas, lista_objetivos,resultado['PJ'])
-#caso1 = armar_mapa(4,2,2,1)
+#caso_mediano_reducido = armar_mapa(5, 5, 4, 3)
